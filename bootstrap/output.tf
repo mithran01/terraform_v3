f8be94088285c624491fdbea399c5c77f3fd83d6
mithran01 <mithran@capminds.com>
2026-05-27 11:09:48 +0530

    [main]:[fix]:[ansible plane output update]

--- bootstrap/output.tf
+++ bootstrap/output.tf
@@ -1,11 +1,15 @@
-output "ansible_plane_ips" {
-  value = aws_instance.ansible_plane[*].private_ip
+# output "ansible_plane_ips" {
+#   value = aws_instance.ansible_plane[*].private_ip
+# }
+
+output "ansible_plane_public_ips" {
+  value = aws_instance.ansible_plane.public_ip
 }
 
 output "control_plane_ips" {
-  value = aws_instance.control_plane[*].private_ip
+  value = aws_instance.control_plane.private_ip
 }
 
 output "ansible_plane_instance_id" {
-  value = aws_instance.ansible_plane[*].id
+  value = aws_instance.ansible_plane.id
 }
