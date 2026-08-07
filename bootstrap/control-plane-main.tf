f58c59c2865bc03c34e5f6b71554983c24ed6bde
mithran01 <mithran@capminds.com>
2026-08-07 14:47:52 +0530

    [main]:[update]:[bootstrap]

--- bootstrap/control-plane-main.tf
+++ bootstrap/control-plane-main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "control_plane" {
   count                       = 1
   ami                         = data.aws_ami.rocky_linux.id
-  instance_type               = "t3.medium"
+  instance_type               = "t3a.medium"
   subnet_id                   = data.aws_subnet.subnet_us_east_1a.id
   key_name                    = aws_key_pair.demov2.key_name
   iam_instance_profile        = aws_iam_instance_profile.kubeadm_profile_01.name
